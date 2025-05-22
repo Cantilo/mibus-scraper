f4bdc9a7a2dea2d6ec6766010b40c75429ba93c1
Cantilo <cantpazveg@gmail.com>
2025-05-22 17:12:10 +0100

    fix(unique_paradas): remove route related information from the unique paradas file

--- unique_paradas.py
+++ unique_paradas.py
@@ -1,16 +1,18 @@
 import json
 
-if __name__ == '__main__':
-    with open('paradas.json', 'r', encoding='utf8') as f:
+if __name__ == "__main__":
+    with open("paradas.json", "r", encoding="utf8") as f:
         paradas: list[dict] = json.load(f)
 
     seen = set()
     unique_paradas = []
     for p in paradas:
-        if not p['id_code'] in seen:
-            seen.add(p['id_code'])
+        if p["id_code"] not in seen:
+            seen.add(p["id_code"])
+            del p["parada_index"]
+            del p["route_id"]
             unique_paradas.append(p)
 
-    print('Total de paradas:', len(unique_paradas))
-    with open('unique_paradas.json', 'w', encoding='utf8') as f:
+    print("Numero de paradas:", len(unique_paradas))
+    with open("unique_paradas.json", "w", encoding="utf8") as f:
         json.dump(unique_paradas, f)
